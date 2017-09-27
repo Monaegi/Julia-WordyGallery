fea3f1e85a59a91c0c97c754766169e324d799f9
JuliaHwang <qufskan9396@gmail.com>
2017-09-28 02:01:42 +0900

    create api: facebook login

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,8 +1,12 @@
+import re
+
+import requests
 from django.conf import settings
 from django.contrib.auth import models as auth_models, get_user_model
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
+from django.core.files.temp import NamedTemporaryFile
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -10,13 +14,13 @@ from rest_framework.authtoken.models import Token
 
 from utils.fields.custom_imagefields import CustomImageField
 
-### 기본 기능 ###
-# 사용자 로그인 - 일반 / 페이스북
-# 회원가입
-# 로그아웃
-# 프로필 정보변경
-# 계정탈퇴
-###############
+############# 기본 기능 #############
+# [완료] 사용자 로그인 - 일반 / 페이스북 #
+# [완료] 회원가입                    #
+# [완료] 로그아웃                    #
+# [완료] 프로필 정보변경               #
+# [완료] 계정탈퇴(삭제)               #
+###################################
 
 __all__ = (
     'MyUser'
@@ -25,8 +29,10 @@ __all__ = (
 
 # 커스텀 사용자 생성 매니저
 class MyUserManager(BaseUserManager):
-    # 일반사용자 생성 메서드
     def create_user(self, username, name, email=None, password=None, **extra_fields):
+        """
+        일반사용자 생성 메서드
+        """
         try:
             user = self.model(
                 user_type=User.USER_TYPE_DJANGO,
@@ -43,8 +49,10 @@ class MyUserManager(BaseUserManager):
         except ValidationError:
             raise ValidationError({'detail': 'Enter a proper Email Account'})
 
-    # 관리자 생성 메서드
     def create_superuser(self, username, name, email=None, password=None, **extra_fields):
+        """
+        관리자 생성 메서드
+        """
         try:
             superuser = self.create_user(
                 user_type=User.USER_TYPE_DJANGO,
@@ -59,6 +67,43 @@ class MyUserManager(BaseUserManager):
             return superuser
         except ValidationError:
             raise ValidationError({"detail": "Enter a proper Email Account"})
+
+    def get_or_create_facebook_user(self, user_info):
+        """
+        페이스북 로그인시 데이터베이스에 받아온 사용자정보로 객체 생성
+        """
+        fb_user, user_created = self.get_or_create(
+            user_type=User.USER_TYPE_FACEBOOK,
+            username=user_info['id'],
+            name=user_info['first_name'] + user_info['last_name'],
+            email=user_info['email']
+        )
+
+        # 유저가 생성된 경우 페이스북의 프로필 이미지를 가져온다.
+        if user_created and user_info.get('picture'):
+            url_profile = user_info['picture']['data']['url']
+
+            # 이미지 확장자와 일치하는 정규표현식
+            p = re.compile(r'.*\.([^?]+)')
+            # 받아온 url_profile을 정규표현식과 일치하는 패턴으로 검사해 이미지명을 저장
+            img_ext = re.search(p, url_profile).group(1)
+            img_name = '{}.{}'.format(
+                fb_user.pk,
+                img_ext
+            )
+
+            # 이미지 파일을 임시로 저장할 파일객체 할당
+            temp_img = NamedTemporaryFile()
+            # 이미지 다운로드
+            response = requests.get(url_profile)
+            # 임시 파일객체에 받아온 이미지를 저장
+            temp_img.write(response.content)
+            # 페이스북 사용자의 이미지를 저장한 임시 파일객체로 저장
+            fb_user.img_profile.save(
+                'fb_prof_{}'.format(
+                    img_name), temp_img)
+            # 페이스북 사용자 반환
+            return fb_user
 
 
 # 전체서비스 내 커스텀유저 모델
